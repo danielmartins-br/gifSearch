4deb15cd0452da058fb54845496994fee9a729e9
Daniel <danielm77881@gmail.com>
2020-10-23 10:41:20 -0300

    Criada barra de rolagem lateral e opcao de copiar a url do Gif

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-const { FlexLayout, QMainWindow, QLabel, QMovie, QWidget, QLineEdit, QPushButton } = require('@nodegui/nodegui');
+const { 
+    ButtonRole, 
+    FlexLayout,
+    QApplication, 
+    QClipboardMode,
+    QLabel, 
+    QLineEdit,
+    QMainWindow, 
+    QMessageBox,
+    QMovie, 
+    QPushButton,
+    QScrollArea, 
+    QWidget,
+    WidgetEventTypes,
+    } = require('@nodegui/nodegui');
+
 const axios = require('axios').default;
 const  GIPHY_API_KEY = 'p77gARnazhGfm7vEfTRx8Spqvj2i2pOk';
 
@@ -8,23 +23,23 @@ async function getMovie(url){
     movie.loadFromData(data);
     movie.start();
     return movie;
-}
+};
 
 async function searchGifs(searchTerm) {
     const url = 'https://api.giphy.com/v1/gifs/search';
     const res = await axios.get(url, {
         params: {
             api_key: GIPHY_API_KEY,
-            limit: 18,
+            limit: 25,
             q: searchTerm,
             lang: 'en',
             offset: 0,
-            rating: 'g'
+            rating: 'pg-13'
         }
     });
 
     return res.data.data;
-}
+};
 
 async function  getGifViews (listOfGifs) {
     const container = new QWidget();
@@ -36,21 +51,32 @@ async function  getGifViews (listOfGifs) {
         const gifView = new QLabel();
         gifView.setMovie(movie);
         gifView.setInlineStyle(`width: ${width}`);
+
+        gifView.addEventListener (WidgetEventTypes.MouseButtonRelease, () =>{
+            const clipboard = QApplication.clipboard();
+            clipboard.setText(url, QClipboardMode.Clipboard);
+
+            showModal(
+                'Copiado para a área de Transferência!',
+                `Aperte ctrl + v para colar a url do GIF: ${url}`
+            );
+        });
+
         container.layout.addWidget(gifView);
     });
 
+    
     await Promise.all(promises);
 
     container.setInlineStyle(`
         flex-direction: 'row';
         flex-wrap: 'wrap';
-        justify-content: 'center';
+        justify-content: 'space-around';
         width: 430px;
-        height: 350px;
     `);
 
     return container;
-}
+};
 
 function createSearchContainer (onSearch) {
     const searchContainer = new QWidget();
@@ -91,7 +117,17 @@ function createSearchContainer (onSearch) {
         `);
 
         return searchContainer;
-}
+};
+
+function showModal (title,details) {
+    const modal = new QMessageBox();
+    modal.setText(title);
+    modal.setDetailedText(details);
+    const okButton = new QPushButton();
+    okButton.setText('OK');
+    modal.addButton(okButton, ButtonRole.AcceptRole);
+    modal.exec();
+};
 
 const main = async() => {
     const win = new QMainWindow();
@@ -100,20 +136,22 @@ const main = async() => {
     const center = new QWidget();
     center.setLayout( new FlexLayout());
 
-    let container = new QWidget();
+    const scrollArea = new QScrollArea();
+    scrollArea.setWidgetResizable(false);
+    scrollArea.setInlineStyle('flex: 1; width: 350px; height: 400px');
+
     const searchContainer = createSearchContainer(async searchText => {
         try {
             //Cria um novo container com uma lista  de novos Gifs
             const listOfGifs = await searchGifs(searchText);
             const newGifContainer = await getGifViews(listOfGifs);
 
-            //Remove os containers existentes da janela
-            center.layout.removeWidget(container);
-            container.close();
+            //Remove os containers existentes na ScrollArea
+            const oldContainer = scrollArea.takeWidget();
+            if (oldContainer) oldContainer.close();
 
-            //Adiciona novos containers de Gifs na Janela
-            center.layout.addWidget(newGifContainer);
-            container = newGifContainer;
+            //Adiciona novos containers de Gifs na ScrollArea
+            scrollArea.setWidget(newGifContainer);
         }
         catch (err) {
             console.error('Algo de errado não está certo', err);
@@ -121,6 +159,7 @@ const main = async() => {
     });
 
     center.layout.addWidget(searchContainer);
+    center.layout.addWidget(scrollArea);
 
     win.setCentralWidget(center);
     win.show();
